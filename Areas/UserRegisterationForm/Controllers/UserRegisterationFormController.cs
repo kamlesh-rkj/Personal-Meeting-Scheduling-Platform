7deacbcca6d1a02ed7d83561122ed72dd92cc57c
KirtanManek <manekkirtan@gmail.com>
2023-10-07 12:04:23 +0530

    added registeration form

---
+++ Areas/UserRegisterationForm/Controllers/UserRegisterationFormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Personal_Meeting_Scheduling_Platform.Areas.UserRegisterationForm.Controllers
+{
+	public class UserRegisterationFormController : Controller
+	{
+		public IActionResult UserRegisterationForm()
+		{
+			return View();
+		}
+	}
+}
