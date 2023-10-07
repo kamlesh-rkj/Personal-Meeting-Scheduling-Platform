fdc3d718ba57ac8817f1f7066bb0ee069af7169a
kamlesh_patel <rkjrk514@gmail.com>
2023-10-07 11:08:42 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Personal_Meeting_Scheduling_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
